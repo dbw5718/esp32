8136e8f103443c9fc238969c75bb2b5d74460350
tachyon <dbw5718@126.com>
2025-07-17 21:13:33 +0800

    修改wifi

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,8 @@
 
 
 //定义两个字符串指针常量
-const char* id="Mi 10S";            //wifi名称
-const char* psw="xwh123456@";       //wifi密码
+const char* name="301";            //wifi名称
+const char* passwd="Chrome...1314@";       //wifi密码
 
 
   void setup() 
@@ -28,7 +28,7 @@ const char* psw="xwh123456@";       //wifi密码
 
     Serial.begin(9600);
 
-    WiFi.begin(id,psw); 
+    WiFi.begin(name,passwd); 
 
     tft.print("wifi connecting");
     while(WiFi.status()!=WL_CONNECTED)      //未连接时阻塞程序，直到连接成功
