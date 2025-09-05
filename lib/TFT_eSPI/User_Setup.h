817b54607f40af2284c31270e9fa800f0c17b7b0
wang.mingzhen <wang.mingzhen@hellogroup.com>
2025-09-05 19:08:55 +0800

    触摸屏

--- lib/TFT_eSPI/User_Setup.h
+++ lib/TFT_eSPI/User_Setup.h
@@ -42,7 +42,7 @@
 //#define RPI_DISPLAY_TYPE // 20MHz maximum SPI
 
 // Only define one driver, the other ones must be commented out
-//#define ILI9341_DRIVER       // Generic driver for common displays
+#define ILI9341_DRIVER       // Generic driver for common displays
 //#define ILI9341_2_DRIVER     // Alternative ILI9341 driver, see https://github.com/Bodmer/TFT_eSPI/issues/1172
 //#define ST7735_DRIVER      // Define additional parameters below for this display
 //#define ILI9163_DRIVER     // Define additional parameters below for this display
@@ -52,7 +52,7 @@
 //#define ILI9481_DRIVER
 //#define ILI9486_DRIVER
 //#define ILI9488_DRIVER     // WARNING: Do not connect ILI9488 display SDO to MISO if other devices share the SPI bus (TFT SDO does NOT tristate when CS is high)
-#define ST7789_DRIVER      // Full configuration option, define additional parameters below for this display
+//#define ST7789_DRIVER      // Full configuration option, define additional parameters below for this display
 //#define ST7789_2_DRIVER    // Minimal configuration option, define additional parameters below for this display
 //#define R61581_DRIVER
 //#define RM68140_DRIVER
@@ -73,8 +73,8 @@
 // For ST7735, ST7789 and ILI9341 ONLY, define the colour order IF the blue and red are swapped on your display
 // Try ONE option at a time to find the correct colour order for your display
 
-//  #define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
-//#define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
+#define TFT_RGB_ORDER TFT_RGB  // Colour order Red-Green-Blue
+//  #define TFT_RGB_ORDER TFT_BGR  // Colour order Blue-Green-Red
 
 // For M5Stack ESP32 module with integrated ILI9341 display ONLY, remove // in line below
 
@@ -88,8 +88,8 @@
 #define TFT_WIDTH  240 // ST7789 240 x 240 and 240 x 320
 // #define TFT_HEIGHT 160
 // #define TFT_HEIGHT 128
-#define TFT_HEIGHT 240 // ST7789 240 x 240
-// #define TFT_HEIGHT 320 // ST7789 240 x 320
+//#define TFT_HEIGHT 240 // ST7789 240 x 240
+#define TFT_HEIGHT 320 // ST7789 240 x 320
 // #define TFT_HEIGHT 240 // GC9A01 240 x 240
 
 // For ST7735 ONLY, define the type of display, originally this was based on the
@@ -210,11 +210,11 @@
 // The hardware SPI can be mapped to any pins
 
 //#define TFT_MISO 19
-//#define TFT_MOSI 23
-//#define TFT_SCLK 18
-//#define TFT_CS   15  // Chip select control pin
-//#define TFT_DC    2  // Data Command control pin
-//#define TFT_RST   4  // Reset pin (could connect to RST pin)
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define TFT_CS   5  // Chip select control pin
+#define TFT_DC    16  // Data Command control pin
+#define TFT_RST   4  // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to ESP32 board RST
 
 // SPI硬件引脚（自定义映射）
@@ -238,17 +238,17 @@
 
 // #define TFT_BL   18    // 背光控制引脚（BLK = Backlight）
 // #define TFT_BACKLIGHT_ON LOW  // 高电平点亮背光（根据硬件调整）
-#define TFT_BL   18    // 背光控制引脚（BLK = Backlight）
+#define TFT_BL   15    // 背光控制引脚（BLK = Backlight）
 #define TFT_BACKLIGHT_ON HIGH  // 高电平点亮背光（根据硬件调整）
 
 // For ESP32 Dev board (only tested with GC9A01 display)
 // The hardware SPI can be mapped to any pins
 
-#define TFT_MOSI 2 // In some display driver board, it might be written as "SDA" and so on.
-#define TFT_SCLK 15
-#define TFT_CS   5  // Chip select control pin
-#define TFT_DC   16  // Data Command control pin
-#define TFT_RST  4  // Reset pin (could connect to Arduino RESET pin)
+// #define TFT_MOSI 2 // In some display driver board, it might be written as "SDA" and so on.
+// #define TFT_SCLK 15
+// #define TFT_CS   5  // Chip select control pin
+// #define TFT_DC   16  // Data Command control pin
+// #define TFT_RST  4  // Reset pin (could connect to Arduino RESET pin)
 
 //#define TOUCH_CS 0    // Chip select pin (T_CS) of touch screen
 
