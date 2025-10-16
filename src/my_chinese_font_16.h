373d2c4b23a11908b70fdd11d9bb701975bfdf32
wang.mingzhen <wang.mingzhen@hellogroup.com>
2025-10-16 14:53:21 +0800

    删除无用代码

--- src/my_chinese_font_16.h
+++
@@ -1,9 +0,0 @@
-#ifndef MY_CHINESE_FONT_16_H
-#define MY_CHINESE_FONT_16_H
-
-#include "lvgl.h"  // 包含LVGL核心头文件
-
-// 声明字体变量（与.c文件中的变量名完全一致）
-LV_FONT_DECLARE(my_chinese_font_16);
-
-#endif // MY_CHINESE_FONT_16_H
